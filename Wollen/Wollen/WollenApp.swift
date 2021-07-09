702663d0e450a1dce5d32034af80ce1d37563df9
José Henrique Fernandes Silva <jhfs@cin.ufpe.br>
2021-07-09 16:19:49 -0300

    Adiciona projeto base do Xcode

---
+++ Wollen/Wollen/WollenApp.swift
@@ -0,0 +1,20 @@
+//
+//  WollenApp.swift
+//  Wollen
+//
+//  Created by José Henrique Fernandes Silva on 09/07/21.
+//
+
+import SwiftUI
+
+@main
+struct WollenApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
