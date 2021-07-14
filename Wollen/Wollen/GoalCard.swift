0977beccd0badd288806e638e629b9585e57d5f6
José Henrique Fernandes Silva <jhfs@cin.ufpe.br>
2021-07-13 23:31:11 -0300

    Adiciona front end do card Meta da tela Adicionar Desejo

--- Wollen/Wollen/GoalCard.swift
+++ Wollen/Wollen/GoalCard.swift
@@ -15,54 +15,127 @@ struct GoalCard: View {
     
     @State private var goalBy: GoalBy = .value
     @State private var frequency: FrequencyTypes = .weekly
+    @State private var duration: DurationTypes = .weeks
     @State private var valueTextField = ""
+    @State private var quantityDuration = 1
     
     var body: some View {
         VStack (alignment: .leading) {
-            Text("Meta")
-                .font(.body)
-                .fontWeight(.bold)
-                .multilineTextAlignment(.leading)
-                .padding()
             
+            HStack {
+                Text("Meta")
+                    .font(.custom("Avenir_Next", size: 22))
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.leading)
+                
+                Spacer()
+                
+                Button(action: {
+                    // TODO: Ação do botão de informação
+                },
+                       label: {
+                    Image(systemName: "info.circle")
+                        .foregroundColor(Color.gray)
+                })
+            }
             
             Picker( selection: $goalBy, label: Text("Que tipo de meta você prefere?")) {
                 // Link para tutorial:
                 //https://www.youtube.com/watch?v=iqKmgs_0VPE
                 ForEach(GoalBy.allCases, id: \.self) {
                     Text($0.rawValue)
+                        .font(.custom("Avenir_Next", size: 18))
+                        .fontWeight(.medium)
+                        .multilineTextAlignment(.leading)
                 }
             }
             .pickerStyle(SegmentedPickerStyle())
             .padding()
+            .fixedSize(horizontal: false, vertical: false)
             
             HStack {
-                Text("Frequência")
+                Text("Frequência:")
+                    .font(.custom("Avenir_Next", size: 18))
+                    .fontWeight(.regular)
+                    .multilineTextAlignment(.leading)
                 
-                Picker(selection: $frequency, label: Text("Qual a frequência que você deseja guardar dinheiro")) {
+                Spacer()
+                
+                Picker(selection: $frequency,
+                       label:
+                        Text(frequency.rawValue)
+                        .foregroundColor(.gray)
+                        .padding(5)
+                        .padding(.horizontal)
+                        .background(Color(red: 118/256, green: 118/256, blue: 128/256, opacity: 0.12))
+                        .cornerRadius(10)
+                        .multilineTextAlignment(.center)
+                        .font(.custom("Avenir_Next", size: 16))) {
+                    
                     ForEach(FrequencyTypes.allCases, id: \.self) {
                         Text($0.rawValue)
                     }
-                }
+                    
+                }.pickerStyle(MenuPickerStyle())
             } // HStack
-            .padding()
             
             HStack {
                 if goalBy == GoalBy.value {
-                    Text("Valor")
+                    Text("Valor:")
+                        .font(.custom("Avenir_Next", size: 18))
+                        .fontWeight(.regular)
+                        .multilineTextAlignment(.leading)
+                    
+                    Spacer()
+                    Spacer()
                     
                     TextField("R$ 4,00", text: $valueTextField)
-                        .background(Color(red: 0.933, green: 0.933, blue: 0.938))
-                        .cornerRadius(6)
+                        .foregroundColor(.gray)
+                        .padding(5)
+                        .background(Color(red: 118/256, green: 118/256, blue: 128/256, opacity: 0.12))
+                        .cornerRadius(10)
                         .multilineTextAlignment(.center)
+                        .font(.custom("Avenir_Next", size: 16))
                     
                 } else {
-                    Text("Duração")
+                    Text("Duração:")
+                    
+                    Spacer()
                     
                     // Picker duplo
+                    Picker(selection: $quantityDuration,
+                           label: Text("\(quantityDuration)").foregroundColor(.gray)
+                            .padding(5)
+                            .padding(.horizontal)
+                            .background(Color(red: 118/256, green: 118/256, blue: 128/256, opacity: 0.12))
+                            .cornerRadius(10)
+                            .multilineTextAlignment(.center)
+                            .font(.custom("Avenir", size: 16))) {
+                        ForEach(1...11, id: \.self) { quantity in
+                            Text("\(quantity)")
+                        }
+                    }.pickerStyle(MenuPickerStyle())
+                    
+                    Picker(selection: $duration,
+                           label: Text(duration.rawValue)
+                            .foregroundColor(.gray)
+                            .padding(5)
+                            .padding(.horizontal)
+                            .background(Color(red: 118/256, green: 118/256, blue: 128/256, opacity: 0.12))
+                            .cornerRadius(10)
+                            .multilineTextAlignment(.center)
+                            .font(.custom("Avenir", size: 16))) {
+                        ForEach(DurationTypes.allCases, id: \.self) {
+                            Text($0.rawValue)
+                        }
+                    }.pickerStyle(MenuPickerStyle())
                 }
             } // HStack
         } // VStack
+        .padding(15)
+        .background(Color(red: 248/256, green: 248/256, blue: 248/256))
+        .cornerRadius(10)
+        .fixedSize(horizontal: false, vertical: false)
     } // body
 }
 
@@ -74,6 +147,12 @@ enum GoalBy: String, CaseIterable {
 enum FrequencyTypes: String, CaseIterable{
     case weekly = "semanal";
     case monthly = "mensal";
+}
+
+enum DurationTypes: String, CaseIterable {
+    case weeks = "semanas"
+    case months = "meses"
+    case years = "anos"
 }
 
 struct GoalCard_Previews: PreviewProvider {
